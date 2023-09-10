dce67fdfaaa1a50909f9ec82f0fb34284953a719
tishina26 <s02210569@gse.cs.msu.ru>
2023-09-10 22:54:21 +0300

    task0 done

---
+++ submissions/task0/repos.md
@@ -0,0 +1,1 @@
+|311|Тишина Ульяна Кирилловна|https://github.com/tishina26/prac-io-0.git|
---
+++ submissions/task0/tishina/program0.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {    
+    int n, a;
+    scanf("%d%d", &n, &a);
+
+    int max = a;
+    int cnt = 1;
+    
+    for (int i = 1; i < n; i++) {
+        scanf("%d", &a);
+        
+        if (a > max) {
+            max = a;
+            cnt = 1;
+        } else if (a == max) {
+            cnt++;
+        }
+    }
+    
+    printf("%d", cnt);
+    
+    return 0;
+}
